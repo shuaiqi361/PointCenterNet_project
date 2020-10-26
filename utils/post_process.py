49f2fac862f889e5665f6090838a412a2356ba36
shuaiqi361 <liukeyi1993@gmail.com>
2020-10-26 17:07:18 -0400

    add segm scale normalization train

--- utils/post_process.py
+++ utils/post_process.py
@@ -101,3 +101,44 @@ def ctsegm_decode(hmap, regs, w_h_, codes_, dictionary, K=100):
     segmentations = torch.cat([segms.view(batch, K, -1), scores, clses], dim=2)
 
     return segmentations
+
+
+def ctsegm_scale_decode(hmap, regs, w_h_, codes_, dictionary, K=100):
+    batch, cat, height, width = hmap.shape
+    hmap = torch.sigmoid(hmap)
+
+    # if flip test
+    if batch > 1:
+        hmap = (hmap[0:1] + flip_tensor(hmap[1:2])) / 2
+        w_h_ = (w_h_[0:1] + flip_tensor(w_h_[1:2])) / 2
+        regs = regs[0:1]
+        codes_ = codes_[0:1]
+
+    batch = 1
+
+    hmap = _nms(hmap)  # perform nms on heatmaps
+
+    scores, inds, clses, ys, xs = _topk(hmap, K=K)
+
+    regs = _tranpose_and_gather_feature(regs, inds)
+    regs = regs.view(batch, K, 2)
+    xs = xs.view(batch, K, 1) + regs[:, :, 0:1]
+    ys = ys.view(batch, K, 1) + regs[:, :, 1:2]
+
+    w_h_ = _tranpose_and_gather_feature(w_h_, inds)
+    w_h_ = w_h_.view(batch, K, 2)
+
+    codes_ = _tranpose_and_gather_feature(codes_, inds)
+    codes_ = codes_.view(batch, K, 64)
+
+    clses = clses.view(batch, K, 1).float()
+    scores = scores.view(batch, K, 1)
+
+    segms = torch.matmul(codes_, dictionary)
+    # print('Sizes:', segms.size(), std_.size(), xs.size())
+    segms = segms.view(batch, K, 32, 2) * w_h_.view(batch, K, 1, 2) / 2. \
+            + torch.cat([xs, ys], dim=2).view(batch, K, 1, 2)
+    segmentations = torch.cat([segms.view(batch, K, -1), scores, clses], dim=2)
+
+    return segmentations
+
