ebcc9d0527da347f7d1e2c677823b0483a1cb5e4
shuaiqi361 <liukeyi1993@gmail.com>
2021-02-02 11:11:32 -0500

    getattr setattr

--- nets/pre_activation_code.py
+++ nets/pre_activation_code.py
@@ -14,36 +14,37 @@ def fill_fc_weights(layers):
 class PreActResidualModule(nn.Module):
     def __init__(self, inplanes, head_conv, outplanes, num_blocks):
         super(PreActResidualModule, self).__init__()
-        self.residual_convs = []
-        self.output_convs = []
+
         self.input_conv = nn.Sequential(nn.Conv2d(inplanes, head_conv, kernel_size=1, padding=0, bias=False),
                                         nn.BatchNorm2d(head_conv),
                                         nn.ReLU(inplace=True),
                                         nn.Conv2d(head_conv, head_conv, kernel_size=3, padding=1, bias=False))
 
         for i in range(num_blocks):
-            self.residual_convs.append(nn.Sequential(nn.BatchNorm2d(head_conv),
-                                                     nn.ReLU(inplace=True),
-                                                     nn.Conv2d(head_conv, head_conv // 2, kernel_size=1, padding=0, bias=False),
-                                                     nn.BatchNorm2d(head_conv // 2),
-                                                     nn.ReLU(inplace=True),
-                                                     nn.Conv2d(head_conv // 2, head_conv, kernel_size=3, padding=1, bias=False)))
-            self.output_convs.append(nn.Sequential(nn.BatchNorm2d(head_conv),
-                                                   nn.ReLU(inplace=True),
-                                                   nn.Conv2d(head_conv, outplanes, kernel_size=1, padding=0, bias=True)))
+            res = nn.Sequential(nn.BatchNorm2d(head_conv),
+                                nn.ReLU(inplace=True),
+                                nn.Conv2d(head_conv, head_conv // 2, kernel_size=1, padding=0, bias=False),
+                                nn.BatchNorm2d(head_conv // 2),
+                                nn.ReLU(inplace=True),
+                                nn.Conv2d(head_conv // 2, head_conv, kernel_size=3, padding=1, bias=False))
+            outs = nn.Sequential(nn.BatchNorm2d(head_conv),
+                                 nn.ReLU(inplace=True),
+                                 nn.Conv2d(head_conv, outplanes, kernel_size=1, padding=0, bias=True))
 
-        for l in self.residual_convs:
-            fill_fc_weights(l)
+            fill_fc_weights(res)
+            fill_fc_weights(outs)
 
-        for l in self.output_convs:
-            fill_fc_weights(l)
+            setattr(self, 'residual_code_' + str(i), res)
+            setattr(self, 'output_code_' + str(i), outs)
 
     def forward(self, x):
         x = self.input_conv(x)
 
         out = []
         for i in range(len(self.output_convs)):
-            x = x + self.residual_convs[i](x)
-            out.append(self.output_convs[i](x))
+            res = getattr(self, 'residual_code_' + str(i))
+            outs = getattr(self, 'output_code_' + str(i))
+            x = x + res(x)
+            out.append(outs(x))
 
         return out
